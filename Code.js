dbc116efef5c24c806cb2abf11ef238e35fb30f3
122Jdavis <56303955+122Jdavis@users.noreply.github.com>
2021-02-17 10:03:10 -0500

    Initial commit

---
+++ Code.js
@@ -0,0 +1,21 @@
+function yellow() {
+  document.getElementById("case").src = "img/yellowphone.jpg";
+  console.log(yellow);
+}
+
+function Pink() {
+  document.getElementById("case").src = "img/pinkphone.jpg";
+}
+
+function White() {
+  document.getElementById("case").src = "img/whitephone.jpg";
+}
+
+function Red() {
+  document.getElementById("case").src = "img/redphone.jpg";
+}
+
+function Black() {
+  document.getElementById("case").src = "img/blackphone.jpg";
+  console.log(yellow);
+}
